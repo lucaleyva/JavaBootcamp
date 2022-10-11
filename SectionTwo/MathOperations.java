7df4ed291aa7a53d1dd96d9128033f177af68748
Luca Leyva <imlucaleyva@gmail.com>
2022-10-10 19:12:52 -0700

    MathOperators - Part 2 Video exercise

--- SectionTwo/MathOperations.java
+++ SectionTwo/MathOperations.java
@@ -25,7 +25,21 @@ public class MathOperations {
     System.out.println("Andromeda has " + (starsInAndromeda / startsInMilkyWay) + " times more stars");
     System.out.println("Snape caught the error. Furious, he divided Ron's score to: " + (testScore / bonusMarks));
 
+    System.out.println("10 is an even number, since dividing 10 by 2 has a remainder of: " + (10 % 2));
+    System.out.println("5 is an odd number, since dividing 5 by 2 has a remainder of: " + (5 % 2));
+
+    int counter = 0;
+    counter++;
+    counter++;
+    counter++;
+    System.out.println(counter);
+
+    counter--;
+    counter--;
+    counter--;
+    System.out.println(counter);
+
+
   }
 }
-
 
