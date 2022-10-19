aeca21f0941eed490cbf054aa72fc1dad6d54525
Luca Leyva <imlucaleyva@gmail.com>
2022-10-19 15:39:34 -0700

    Completed 3.1 and 3.2 sections

---
+++ SectionThree/BooleanComparisons.java
@@ -0,0 +1,24 @@
+public class BooleanComparisons {
+  
+  public static void main(String[] args) {
+    
+    int chemistryGrade = 95;
+    int biologyGrade = 75;
+    int englishGrade = 75;
+    System.out.println(chemistryGrade == englishGrade);
+    System.out.println(biologyGrade == englishGrade);
+    
+    // double sales = 37.55;
+    // double costs = 5.55;
+    // System.out.println(costs > sales);
+
+    String sentence = "I love my son";
+    String sentence2 = "I love my son";
+    System.out.println(sentence.equals(sentence2));
+
+    double value = 3.6;
+    System.out.println((int)value == 3);
+
+  }
+
+}
---
+++ SectionThree/IfElse.java
@@ -0,0 +1,14 @@
+public class IfElse {
+  
+  public static void main(String[] args) {
+    int grade = 45;
+
+    if (grade > 50) {
+      System.out.println("Great job, you passed!");
+    } else {
+      System.out.println("Study harder next time.");
+    }
+
+  }
+  
+}
