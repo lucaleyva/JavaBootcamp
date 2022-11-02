852d1c6986b52897e0c4edd62713533ba0e50e61
Luca Leyva <imlucaleyva@gmail.com>
2022-11-02 14:07:56 -0700

    Created two arrays: apples and prices. Then created a for loop that will print each apple and it's corresponding price.

---
+++ SectionSix_Arrays/Receipt.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Receipt {
+    public static void main(String[] args) {
+
+        String[] apples = {"Gala", "Granny Smith", "Macintosh"};
+        double[] price = {1.99, 1.49, 1.29};
+
+
+        System.out.println("Here's your receipt:\n");
+        /* Task 2
+            Use a for loop to print each apple and the corresponding price.
+            
+            for {
+                println(\t<apple i >: $<price i >)
+            }
+        */
+
+        for (int i = 0; i < apples.length; i++) {
+            System.out.println("\n" + apples[i] + ": $" + price[i]);
+        }
+    }
+}
