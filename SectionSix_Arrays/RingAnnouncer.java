1aa04fbdb854bbc4dbd1d1dedc71be7c1158db40
Luca Leyva <imlucaleyva@gmail.com>
2022-11-02 13:57:09 -0700

    Calculating the fight record of a boxer, and then printing the record (wins and losses).

---
+++ SectionSix_Arrays/RingAnnouncer.java
@@ -0,0 +1,36 @@
+public class RingAnnouncer {
+    public static void main(String[] args) {
+        String[] record = {"WIN", "WIN", "WIN", "LOSS", "WIN", "WIN", "LOSS"};
+
+        /** Task 1 
+         * 
+         *   Using a for loop:
+         *       count Java's wins and store the result in a variable: wins. 
+         *       count Java's losses and store the result in a variable: losses.
+         *
+         * 
+         * 
+         */
+        int wins = 0;
+        int losses = 0;
+         for (int i = 0; i < record.length; i++) {
+            if (record[i].equals("WIN")) {
+                wins += 1;
+            }
+            if (record[i].equals("LOSS")) {
+                losses += 1;
+            }
+         }
+
+        /** Task 2
+         * Print:
+         *     • \nWith a professional record of <wins> Wins and <losses> losses. 
+         *     • He is the pride of oracle: Java Fury!
+         *
+         * 
+         */        
+        System.out.println("\nWith a professional record of " + wins + " Wins and " + losses + " losses.");
+        System.out.println("He is the pride of oracle: Java Fury!");
+
+    }
+}
