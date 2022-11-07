2829c6c27604f4f4923bd5adddf3828c11367a92
Luca Leyva <imlucaleyva@gmail.com>
2022-11-07 05:37:47 -0800

    Javapedia challenge. Create a mock DB where the user can populate information and then search for a person based on their name.

--- SectionSix_Arrays/Javapedia.java
+++ SectionSix_Arrays/Javapedia.java
@@ -5,44 +5,32 @@ public class Javapedia {
         Scanner scan = new Scanner(System.in);
         System.out.println("\n**********Javapedia**********");
         System.out.println("How many historical figures will you register?");
-        //Task 1 – Ask the user: how many historical figures will you register?
-        //       – Store the value.
-        
-        //Task 2 – Create a 2D array with a variable number of rows, and 3 values per row.         
-         
-        //Watch out for the nextLine() pitfall. 
-        /* Task 3 
-        for (____) {
+        int people = scan.nextInt();
+        String[][] database = new String[people][3]; scan.nextLine();
 
+        for (int i = 0; i < database.length; i++) {
             System.out.println("\n\tFigure " + (i+1)); 
-
             System.out.print("\t - Name: ");
-            pick up and store figure's name.   
-
+            database[i][0] = scan.nextLine();    
             System.out.print("\t - Date of birth: ");
-            pick up and store figure's birthday.
-
+            database[i][1] = scan.nextLine();    
             System.out.print("\t - Occupation: ");
-            pick up and store figure's occupation. 
-
+            database[i][2] = scan.nextLine();    
             System.out.print("\n");
-
         }
-        
-        */
-        System.out.println("These are the values you stored:"); 
-        //Task 4: call print2DArray. 
+        System.out.println("These are the values you stored:\n"); 
+        print2DArray(database);
 
-        System.out.print("\nWho do you want information on? ");  
-        
-        /*Task 5: Let the user search the database by name. 
-            If there's a match:
-              print(    tab of space    Name: <name>)
-              print(    tab of space    Date of birth: <date of birth>)
-              print(    tab of space    Occupation: <occupation>)
-
-        */        
-
+        System.out.print("\nWho do you want information on? ");
+        String name = scan.nextLine();  
+        for (int i = 0; i < database.length; i++) {
+            if (database[i][0].equals(name)) {
+                System.out.println("\tName: " + database[i][0]);
+                System.out.println("\tDate of Birth: " + database[i][1]);
+                System.out.println("\tOccupation: " + database[i][2]);
+            }
+        }
+       
         scan.close();
     }
 
@@ -56,5 +44,15 @@ public class Javapedia {
      *     • each value in database has one space from the other value. 
      *     • print a new line.
      */
+
+     public static void print2DArray(String[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("\n");
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println("\n");
+        }
+    }
 
 }
